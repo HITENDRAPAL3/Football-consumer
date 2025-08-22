1f5e267ee0760285b55166b8a9a3d00eeff843b3
HITENDRAPAL3 <hitendrasinghpal3@gmail.com>
2025-08-22 20:22:39 +0530

    Kafka build try catch

--- src/main/java/com/consumer/Consumer/ServiceLayer/logic/PushCommentsToCBStep.java
+++ src/main/java/com/consumer/Consumer/ServiceLayer/logic/PushCommentsToCBStep.java
@@ -3,6 +3,8 @@ package com.consumer.Consumer.ServiceLayer.logic;
 import com.consumer.Consumer.DomainLayer.DTO.MatchCommentDTOs.MatchCommentDTO;
 import com.consumer.Consumer.PersistenceLayer.Couchbase.CommentRepository;
 import com.consumer.Consumer.PersistenceLayer.mapper.IMatchCommentMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
@@ -14,8 +16,15 @@ public class PushCommentsToCBStep {
     @Inject
     private IMatchCommentMapper MatchCommentMapper;
 
+    Logger logger = LoggerFactory.getLogger(PushCommentsToCBStep.class);
+
     public void pushCommentsToCBStep(MatchCommentDTO matchCommentDTO) {
-        commentRepository.save(MatchCommentMapper.map(matchCommentDTO));
+        try {
+            commentRepository.save(MatchCommentMapper.map(matchCommentDTO));
+            logger.info("Comment saved successfully!!!");
+        } catch (Exception e) {
+            logger.error("Failed to persist on couchbase!!!");
+        }
     }
 
 }
